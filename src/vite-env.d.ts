e74d83fd5c9427adee14cfcdbfd62460842ba538
Association-Aide-Médicale <douliacameroun@gmail.com>
2026-02-13 11:54:40 +0100

    suppression de composants ancien

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-
-// Fixed: Provide fallback declarations for Vite environment variables to avoid 'vite/client' not found error.
-interface ImportMetaEnv {
-  readonly [key: string]: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
